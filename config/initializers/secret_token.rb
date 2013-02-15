36e57e4552dd903fa006e2446b9463e8b93e4c95
eric-norcross <eric@base10creations.com>
2013-02-15 08:56:22 -0800

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Martinfurniture::Application.config.secret_token = 'e5651d31410b75108986612c7b9d68785202f2a3f5fc71eb444acd148ac13f1bad755a2e733e7dace4197b55309e8ab42c3db651ef82d9c2fed42cb857a9d94a'
