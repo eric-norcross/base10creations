1448a2468e3406d36811b5f33182fd4c76accfe2
eric-norcross <eric@base10creations.com>
2013-04-03 15:10:33 -0700

    more shiv testing

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,6 +47,7 @@ Martinfurniture::Application.configure do
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
+  config.assets.precompile += ["ie/IE9.css.scss", "ie/IE7.css.scss", "ie/IE8.css.scss", "ie/IE6.css.scss"]
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
