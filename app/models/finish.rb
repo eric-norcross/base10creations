62f410c54ffb3d0b2515019615dd875f6745d3ad
Eric Norcross <eric@base10creations.com>
2013-06-02 10:51:51 -0700

    skis working with finishes

--- app/models/finish.rb
+++ app/models/finish.rb
@@ -6,10 +6,13 @@ class Finish < ActiveRecord::Base
                                 ## belongs_to ##
 
                                 ## has_many ##
+                                :sku,
 
                                 ## polymorphic associations ##
                                 :image_attributes
 
+
+  has_many                      :skus
 
   has_many                      :image, as: :imageable, :dependent => :destroy
   accepts_nested_attributes_for :image, reject_if: proc { |attrs| attrs['asset'].blank? && attrs['asset_cache'].blank? }, allow_destroy: true
