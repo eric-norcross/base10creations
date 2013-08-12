824ff2b4968149709bd97d0150c0bbb1919a65eb
Eric Norcross <eric@base10creations.com>
2013-08-12 11:06:14 -0700

    Added JS callbacks for image uploading

--- app/assets/javascripts/init.js
+++ app/assets/javascripts/init.js
@@ -1,3 +1,6 @@
+var imageUploads;
+var uploadsComplete;
+
 $(document).on("focus", ".clear-input", function(event) {
   if ($(this).val() == $(this).attr("data-message")) {
     $(this).val("");
@@ -9,6 +12,31 @@ $(document).on("blur", ".clear-input", function(event) {
     $(this).val($(this).attr("data-message"));
   }
 });
+
+$(document).on("fileuploadadd", function(event) {
+  var submitButton = $("input[type=submit]");
+  submitButton.attr("disabled", "disabled");
+  submitButton.addClass("disabled")
+  submitButton.attr("value", "Please wait for image(s) to finish uploading.");
+
+  // submitButton.css({ 'opacity' : 0.5 });
+  console.log(submitButton)
+  console.log(imageUploads.length)
+  imageUploads.push(event.target)
+  console.log("Added file to " + event.target)
+})
+
+$(document).on("fileuploaddone", function(event) {
+  var submitButton = $("input[type=submit]");
+  submitButton.removeAttr('disabled');
+  submitButton.removeClass("disabled")
+  submitButton.attr("value", "Save");
+
+  uploadsComplete.push(event.target)
+  if (imageUploads.length == uploadsComplete.length) {
+    console.log("Ok to save!")
+  }
+})
 
 $(document).ready(function(){
   $("a.print").click(function(event) {
@@ -25,4 +53,7 @@ $(document).ready(function(){
   });
 
   $('.cloudinary-fileupload').fileupload('option', 'replaceFileInput', false);
+
+  imageUploads = [];
+  uploadsComplete = [];
 });
