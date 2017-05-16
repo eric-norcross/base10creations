da9e0d52683e73a283b5da14158b32e8a613041b
Eric Norcross <eric@base10creations.com>
2017-05-15 17:24:34 -0700

    added resume

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  get "/resume" => redirect("http://res.cloudinary.com/hkowwlpd3/image/upload/v1494542404/eric_norcross_resume3_tc9wmo.pdf")
+  get "/resume" => redirect("http://res.cloudinary.com/hkowwlpd3/image/upload/v1494894187/eric_norcross_resume_k7lwb6.ai")
   root 'application#home'
 end
