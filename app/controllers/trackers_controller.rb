7ca9da5ebf696867d4c5c5d4ee0de7c64339773f
Eric Norcross <eric@base10creations.com>
2017-08-11 16:38:12 -0700

    create tracker record and added index page

--- app/controllers/trackers_controller.rb
+++ app/controllers/trackers_controller.rb
@@ -1,18 +1,7 @@
 class TrackersController < ApplicationController
   # GET /trackers/1
   def show
-    # data = open("http://www.gravatar.com/avatar/c2713a4959692f16d27d2553fb06cc4b.png?r=x&s=170") 
-
-    # data = open(asset_url("colinparent_@1x.jpg"))
-    # send_data data, type: image.content_type, disposition: 'inline'
-
-
-    # [ 200, { 'Content-Type' => 'image/png' }, [ File.read(File.join(File.dirname(__FILE__), 'track.png')) ] ]
-    # send_data image.data, type: image.content_type, disposition: :inline
-
-
-    # send_data File.read("app/assets/images/", mode: "rb"), filename: 'tracker.gif', type: 'image/gif'
-
+    Tracker.create(domain: request.host)
     send_data open(view_context.image_url("tracker.gif")), filename: 'tracker.gif', type: 'image/gif'
   end
 
